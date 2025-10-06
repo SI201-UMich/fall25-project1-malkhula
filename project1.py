5c4c3858ec4c1c95d8272eaab01142d1753d14c1
malkhula <malkhula@umich.edu>
2025-10-06 14:22:24 -0400

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,7 @@
+# SI 201 Project 1
+# Your name: Mariam Alkhulaidi
+# Your student id: 73128025
+# Your email: malkhula@umich.edu
+# Who or what you worked with on this project (including generative AI like ChatGPT): 
+# If you worked with generative AI also add a statement for how you used it.  
+# ""
