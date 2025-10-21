2964a03abc6f98058b53872d030f29c24c507822
malkhula <malkhula@umich.edu>
2025-10-20 22:48:30 -0400

    Testing commits

--- project1.py
+++ project1.py
@@ -8,15 +8,6 @@
 # "I asked Chatgpt hints for debugging and suggesting the general sturcture of the code, 
 # as well as pointing out errors and giving me explanations for when I didn't understand certain code"
 
-
-# SI 201 Project 1
-# Your name: Mariam Alkhulaidi
-# Your student id: 73128025
-# Your email: malkhula@umich.edu
-# Who or what you worked with on this project (including generative AI like ChatGPT): 
-# If you worked with generative AI also add a statement for how you used it.  
-# ""
-
 import csv
 import unittest
 
